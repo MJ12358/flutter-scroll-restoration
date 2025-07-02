f2e1621fe5f04492ca289e0f66bda8ff2296314f
MJ12358 <wtpltlf.dev@gmail.com>
2025-07-01 20:51:22 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# flutter-scroll-restoration
+# Scroll Restoration
+
+- Inspired by [scroll_restore](https://pub.dev/packages/scroll_restore)
+
+## Features
+
+Automatically saves and restores the scroll position of any scrollable widget.
+
+## Usage
+
+```dart
+class MyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Screen')),
+      body: ScrollRestore(
+        id: 'my_restorable_list_id',  // unique key per scrollable
+        builder: (context, controller) {
+          return ListView.builder(
+            controller: controller,
+            itemCount: messages.length,
+            itemBuilder: (_, index) {
+              return ListTile(
+                title: Text(messages[index].text),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+```
---
+++ lib/src/scroll_restoration.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+typedef ScrollRestorationBuilder = Widget Function(
+  BuildContext,
+  ScrollController,
+);
+
+/// {@template ScrollRestoration}
+/// Wrap any scrollable widget to remember its scroll offset.
+/// {@endtemplate}
+class ScrollRestoration extends StatefulWidget {
+  /// {@macro ScrollRestoration}
+  const ScrollRestoration({
+    super.key,
+    required this.id,
+    required this.builder,
+  });
+
+  /// Unique identifier for this scrollable.
+  /// Used as the [SharedPreferences] key.
+  final String id;
+
+  /// Build your scrollable, passing in the provided [ScrollController].
+  final ScrollRestorationBuilder builder;
+
+  @override
+  ScrollRestorationState createState() => ScrollRestorationState();
+}
+
+class ScrollRestorationState extends State<ScrollRestoration> {
+  late final ScrollController _controller;
+  late final SharedPreferences _prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = ScrollController();
+    _initPrefs();
+  }
+
+  Future<void> _initPrefs() async {
+    _prefs = await SharedPreferences.getInstance();
+    final double savedOffset = _prefs.getDouble(widget.id) ?? 0.0;
+
+    // Wait for first frame so controller has a valid position
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!_controller.hasClients) {
+        return;
+      }
+
+      final double maxScroll = _controller.position.maxScrollExtent;
+      final double offset = savedOffset.clamp(0.0, maxScroll);
+      _controller.jumpTo(offset);
+
+      // Start listening to scroll end events after initialization
+      _controller.position.isScrollingNotifier.addListener(_saveOffset);
+    });
+  }
+
+  void _saveOffset() {
+    // Only save when scrolling stops
+    if (!_controller.position.isScrollingNotifier.value) {
+      if (!_controller.hasClients) {
+        return;
+      }
+      _prefs.setDouble(widget.id, _controller.offset);
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.position.isScrollingNotifier.removeListener(_saveOffset);
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.builder(context, _controller);
+  }
+}
